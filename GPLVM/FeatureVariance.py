523a4898a505316afd04bd0732e33e2772a84667
Wilfried Woeber <woeber@technikum-wien.at>
2021-01-19 10:32:26 +0100

    Added default GP-LVM visualizations for publication

--- GPLVM/FeatureVariance.py
+++ GPLVM/FeatureVariance.py
@@ -26,16 +26,50 @@ data_Model	= "./optModel_bgp_model.npy"
 data_latent	= "./optModel_bgp_features_train.csv"
 dataFolder	= "../Data/design.csv"
 #--- Create bGPLVM model ---#
-model =bGPLVM(  dataFolder,         #Path to training data
-                data_latent,        #Extracted features
-              	data_Model,         #Path to model data
-                "",                 #Sampels from faulty classes
-                "",                 #Path to excluded featues
-                nrInd,              #Number of inducing pts
-                latentDim,          #Number of latent dimensions
-                (image_dim[1],image_dim[0]))       #Reshaping image
+#model =bGPLVM(  dataFolder,         #Path to training data
+#                data_latent,        #Extracted features
+#              	data_Model,         #Path to model data
+#                "",                 #Sampels from faulty classes
+#                "",                 #Path to excluded featues
+#                nrInd,              #Number of inducing pts
+#                latentDim,          #Number of latent dimensions
+#                (image_dim[1],image_dim[0]))       #Reshaping image
 #--------------------------------------#
 #--- Estimate variance per features ---#
 #--------------------------------------#
-os.system("mkdir Heatmaps")
-model.plotVarHeatmaps(prefix="./Heatmaps/")
+#os.system("mkdir Heatmaps")
+#model.plotVarHeatmaps(prefix="./Heatmaps/")
+#-------------------------------#
+#--- Create summarized plots ---#
+#-------------------------------#
+plot_top_n=(3,4)
+selection = np.loadtxt("../Data/unselectedFeatures.csv", dtype=int)
+f, arr = plt.subplots(plot_top_n[0],plot_top_n[1])  #Create 'grid' for plot
+x_looper=0
+y_looper=0
+for i in range(0,np.prod(plot_top_n)):
+    img=np.loadtxt('./Heatmaps/'+str(selection[i])+'.csv',delimiter=',')
+    arr[y_looper,x_looper].imshow(img,cmap='jet')
+    arr[y_looper,x_looper].text(5,30, "F"+str(selection[i]),fontsize=20,color='red')
+    arr[y_looper,x_looper].axis('off')
+    x_looper=x_looper+1
+    if(x_looper >= plot_top_n[1]):
+        x_looper=0
+        y_looper=y_looper+1
+    plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+plt.savefig("./Heatmaps/selection.pdf",bbox_inches = 'tight',pad_inches = 0)
+plt.savefig("./Heatmaps/selection.png",bbox_inches = 'tight',pad_inches = 0)
+#----------------------------------#
+#--- sampe for ''bad'' features ---#
+#----------------------------------#
+selection = [bad for bad in np.array(range(0,9)) if not np.isin(bad,selection)]
+f, arr = plt.subplots(1,len(selection))  #Create 'grid' for plot
+for i in range(0,len(selection)):
+    img=np.loadtxt('./Heatmaps/'+str(selection[i])+'.csv',delimiter=',')
+    arr[i].imshow(img,cmap='jet')
+    arr[i].text(5,30, "F"+str(selection[i]),fontsize=20,color='red')
+    arr[i].axis('off')
+plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+plt.savefig("./Heatmaps/technicalBackground.pdf",bbox_inches = 'tight',pad_inches = 0)
+
