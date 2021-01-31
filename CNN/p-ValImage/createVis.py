656174d4c95ae398cb291af17cb4ab4884e568a2
Wilfried Woeber <woeber@technikum-wien.at>
2021-01-31 17:55:28 +0100

    Update visualization script to use functions

--- CNN/p-ValImage/createVis.py
+++ CNN/p-ValImage/createVis.py
@@ -63,80 +63,81 @@ def addBorder(img):
 #--------------------------#
 #--- Do main processing ---#
 #--------------------------#
-if(len(sys.argv)==1):
-    sys.argv=('','../Classification/Augmented/','GRAD')
-path_data= sys.argv[1]	    #Path to CNN data
-if(sys.argv[2] == "GRAD"):
-    doGRAD=True     #Check if Grad-CAM should be used
-elif(sys.argv[2] == "LRP"):
-    doGRAD=False
-else:
-    print("ERROR: choose appropriate visualization")
-    sys.exit(-1)
-pImg_k=1000                 #Number of samples for p img creation
-alpha = 0.001               #Alpha value for test
-#---------------------------#
-#--- Print system config ---#
-#---------------------------#
-print("Processing folger %s"%path_data)
-print("Use kernel width/height %d" % kernel_width)
-print("Use std sigma value")
-print("Use %d sampling iterations" % pImg_k)
-print("Using alpha value %f" %alpha)
-print("Doing Grad-CAM %d" %doGRAD)
-#------------------------------#
-#--- Process augmented data ---#
-#------------------------------#
-class_names=("Chamo","Hawassa","Koka","Lan","Tana","Ziway")
-Iteration="0"
-for k in range(0,10):
-    print("Process %d"%k)
-    test_folder=path_data+"test_"+Iteration+"_"+str(k)
-    test_files = [f for f in os.listdir(test_folder) if 
-            os.path.isfile(os.path.join(test_folder, f))]   #Get all files in the test folder
-    img_files_all = [f for f in test_files if (os.path.splitext(f)[1]=='.npy')] #Get the npy images
-    img_files = [f.split('__')[0] for f in img_files_all if len(f.split('LRP'))>1]     #Get specimen names
-    #--- Now filter in best specimen names and all other specimens ---#
-    img_files_bestSpecimen = [f for f in img_files if len(f.split('.'))!=1]
-    img_files_allSpecimen  = [f for f in img_files if len(f.split('.'))==1]
-    #--- Process all images ---#
-    for IMAGE in img_files_allSpecimen:
-        print("  Process %s"%IMAGE)
-        img_name=IMAGE
-        #---------------------------------------#
-        #--- Get all images from data folder ---#
-        #---------------------------------------#
-        img_LRP = np.load(test_folder+"/"+img_name+"__LRP_10.npy")
-        img_GRD = np.load(test_folder+"/"+img_name+"__grad.npy")
-        img_GRD_raw = img_GRD.copy()
-        #--- Conversion from heatmap to grayscale image ---#
-        img_GRD = cv2.cvtColor(img_GRD, cv2.COLOR_RGB2LUV) 
-        img_GRD = cv2.cvtColor(img_GRD, cv2.COLOR_BGR2GRAY) 
-        img     = np.load(test_folder+"/"+img_name+"_.npy")
-        img_RGB = img.copy()
-        img     = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)     #Convert to grayscale
-        #------------------------------------#
-        #--- Do the p-value visualization ---#
-        #------------------------------------#
-        if(doGRAD):
-            pImg=getPimage(img_GRD,pImg_k)  #Get p-value image
-        else:
-            pImg=getPimage(img_LRP,pImg_k)  #Get p-value image
-        #------------------------#
-        #--- Do visualization ---#
-        #------------------------#
-        f, arr = plt.subplots(1,3)
-        arr[0].imshow(img, cmap='gray');arr[0].axis('off')
-        if(doGRAD):
-            arr[1].imshow(img_GRD_raw);arr[1].axis('off')
-        else:
-            arr[1].imshow(img_LRP);arr[1].axis('off')
-        #arr[2].imshow(pImg<alpha, cmap='gray');arr[2].axis('off')
-        img_RGB[:,:,0]=img_RGB[:,:,2]+addBorder(np.multiply(pImg<alpha,img))
-        arr[2].imshow(img_RGB);arr[2].axis('off')
-        #arr[3].imshow(np.multiply(pImg<alpha,img), cmap='gray');arr[3].axis('off')
-        plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
-        #plt.show()
-        plt.savefig(IMAGE+".pdf",bbox_inches = 'tight',pad_inches = 0)
-        plt.savefig(IMAGE+".png",bbox_inches = 'tight',pad_inches = 0)
-        plt.close()
+if __name__ == "__main__":
+    if(len(sys.argv)==1):
+        sys.argv=('','../Classification/Augmented/','GRAD')
+    path_data= sys.argv[1]	    #Path to CNN data
+    if(sys.argv[2] == "GRAD"):
+        doGRAD=True     #Check if Grad-CAM should be used
+    elif(sys.argv[2] == "LRP"):
+        doGRAD=False
+    else:
+        print("ERROR: choose appropriate visualization")
+        sys.exit(-1)
+    pImg_k=1000                 #Number of samples for p img creation
+    alpha = 0.001               #Alpha value for test
+    #---------------------------#
+    #--- Print system config ---#
+    #---------------------------#
+    print("Processing folger %s"%path_data)
+    print("Use kernel width/height %d" % kernel_width)
+    print("Use std sigma value")
+    print("Use %d sampling iterations" % pImg_k)
+    print("Using alpha value %f" %alpha)
+    print("Doing Grad-CAM %d" %doGRAD)
+    #------------------------------#
+    #--- Process augmented data ---#
+    #------------------------------#
+    class_names=("Chamo","Hawassa","Koka","Lan","Tana","Ziway")
+    Iteration="0"
+    for k in range(0,10):
+        print("Process %d"%k)
+        test_folder=path_data+"test_"+Iteration+"_"+str(k)
+        test_files = [f for f in os.listdir(test_folder) if 
+                os.path.isfile(os.path.join(test_folder, f))]   #Get all files in the test folder
+        img_files_all = [f for f in test_files if (os.path.splitext(f)[1]=='.npy')] #Get the npy images
+        img_files = [f.split('__')[0] for f in img_files_all if len(f.split('LRP'))>1]     #Get specimen names
+        #--- Now filter in best specimen names and all other specimens ---#
+        img_files_bestSpecimen = [f for f in img_files if len(f.split('.'))!=1]
+        img_files_allSpecimen  = [f for f in img_files if len(f.split('.'))==1]
+        #--- Process all images ---#
+        for IMAGE in img_files_allSpecimen:
+            print("  Process %s"%IMAGE)
+            img_name=IMAGE
+            #---------------------------------------#
+            #--- Get all images from data folder ---#
+            #---------------------------------------#
+            img_LRP = np.load(test_folder+"/"+img_name+"__LRP_10.npy")
+            img_GRD = np.load(test_folder+"/"+img_name+"__grad.npy")
+            img_GRD_raw = img_GRD.copy()
+            #--- Conversion from heatmap to grayscale image ---#
+            img_GRD = cv2.cvtColor(img_GRD, cv2.COLOR_RGB2LUV) 
+            img_GRD = cv2.cvtColor(img_GRD, cv2.COLOR_BGR2GRAY) 
+            img     = np.load(test_folder+"/"+img_name+"_.npy")
+            img_RGB = img.copy()
+            img     = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)     #Convert to grayscale
+            #------------------------------------#
+            #--- Do the p-value visualization ---#
+            #------------------------------------#
+            if(doGRAD):
+                pImg=getPimage(img_GRD,pImg_k)  #Get p-value image
+            else:
+                pImg=getPimage(img_LRP,pImg_k)  #Get p-value image
+            #------------------------#
+            #--- Do visualization ---#
+            #------------------------#
+            f, arr = plt.subplots(1,3)
+            arr[0].imshow(img, cmap='gray');arr[0].axis('off')
+            if(doGRAD):
+                arr[1].imshow(img_GRD_raw);arr[1].axis('off')
+            else:
+                arr[1].imshow(img_LRP);arr[1].axis('off')
+            #arr[2].imshow(pImg<alpha, cmap='gray');arr[2].axis('off')
+            img_RGB[:,:,0]=img_RGB[:,:,2]+addBorder(np.multiply(pImg<alpha,img))
+            arr[2].imshow(img_RGB);arr[2].axis('off')
+            #arr[3].imshow(np.multiply(pImg<alpha,img), cmap='gray');arr[3].axis('off')
+            plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+            #plt.show()
+            plt.savefig(IMAGE+".pdf",bbox_inches = 'tight',pad_inches = 0)
+            plt.savefig(IMAGE+".png",bbox_inches = 'tight',pad_inches = 0)
+            plt.close()
